806963ab079dcac1473a8870f5372be88d16842d
Yingglish <1085963924@qq.com>
2020-12-14 19:35:31 +0800

    commit

--- 基础/Function.py
+++ 基础/Function.py
@@ -6,7 +6,8 @@ def describe_pet(animal_type , pet_name):
 # 调用
 describe_pet('hamster' , 'harry')
 
-describe_pet(pet_name='tom', animal_type='cat') # 通过关键字实参调用
+# 通过关键字实参调用
+describe_pet(pet_name='tom', animal_type='cat') 
 
 # 传递任意数量的实参 
 # 形参*topping 会让python创建一个名为topping的元祖,并将接收的所有值传入这个元组中
@@ -28,3 +29,19 @@ def build_profile(first, last, **user_info):
 
 user_profile = build_profile('albert', 'einstein', localction = 'princeton', field = 'physics')
 print(user_profile) # output: {'first_name': 'albert', 'last_name': 'einstein', 'localction': 'princeton', 'field': 'physics'}
+
+# 输出一个函数
+def output(name="hi"):
+    return name
+
+print(output()) # output: hi
+
+# 将函数赋值给一个变量
+greet = output
+# 运行
+print(greet()) # output: hi
+
+# 删除 output 函数
+del output
+print(greet()) # output: hi
+print(output()) # output NameError: name 'output' is not defined
